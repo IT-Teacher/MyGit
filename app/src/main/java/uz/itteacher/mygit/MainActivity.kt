9c8892d3ee44ef31a73a03f341f495a779fcb845
IT-Teacher <mirgiyozovm@gmail.com>
2025-10-06 12:43:51 +0500

    SecondScreen

--- app/src/main/java/uz/itteacher/mygit/MainActivity.kt
+++ app/src/main/java/uz/itteacher/mygit/MainActivity.kt
@@ -20,21 +20,12 @@ class MainActivity : ComponentActivity() {
         setContent {
             MyGitTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                  Text(text = "Salom", modifier = Modifier.padding(innerPadding))
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+
 
